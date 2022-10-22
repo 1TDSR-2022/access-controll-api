4b9bc75a494e1a540d1293e7306dcf42206d9a9d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-10-21 22:14:53 -0300

    Menu implementado.

--- pf0670/login/src/App.jsx
+++ pf0670/login/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Menu from "./components/Menu";
+import MainRoutes from "./routes/MainRoutes";
 
 export default function App() {
   return (
     <div>
+      <Menu />
       <h1>CONTROLE DE ACESSO</h1>
+      <MainRoutes />
     </div>
   );
 }
